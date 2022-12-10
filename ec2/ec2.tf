320c1813956e3f98d3778bb92137ce1672a761d0
yembeglo <94507399+yembeglo@users.noreply.github.com>
2022-12-10 14:21:33 -0500

    code improvments

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -61,7 +61,7 @@ resource "aws_iam_instance_profile" "instance_profile" {
 
 
 resource "aws_instance" "web_server" {
-  ami                    = "ami-0b0dcb5067f052a63"
+  ami                    = "ami-0beaa649c482330f7"
   instance_type          = "t3.small"
   key_name               = var.key_pair_name  
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
@@ -71,10 +71,10 @@ resource "aws_instance" "web_server" {
 
   # best practices as per checkov scanner
 
-  # monitoring = true
-  # ebs_optimized = true
-  # root_block_device {
-  # encrypted     = true
-  # }
+   monitoring = true
+   ebs_optimized = true
+   root_block_device {
+   encrypted     = true
+   }
 
 }
--- var.tf
+++ var.tf
@@ -1,7 +1,7 @@
 variable "key_pair_name" {
     type = string
     description = "keypair to utilize"
-    default = "vamsi-chunduru"
+    default = "Jenkins-demo-keypair"
 }
 
 # variable "vpc_id" {
